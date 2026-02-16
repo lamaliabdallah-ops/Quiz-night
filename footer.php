3eaadf1e0c9dca68a65fbb003d3da2e9f6baaf20
lamaliabdallah <lamaliabdalah90@gmail.com>
2026-02-16 19:39:28 +0100

    page index

---
+++ footer.php
@@ -0,0 +1,8 @@
+</div>
+
+<footer style="background:#333; color:white; text-align:center; padding:10px;">
+    <p>© 2026 - Projet Quiz</p>
+</footer>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Mon Site Quiz</title>
+    <style>
+        body {
+            font-family: Arial;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #333;
+            color: white;
+            padding: 15px;
+            text-align: center;
+        }
+
+        nav a {
+            color: white;
+            margin: 0 10px;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+
+<header>
+    <nav>
+        <a href=>Accueil</a>
+        <a href=>connexion</a>
+        <a href=>inscription</a>
+        <a href=>profil</a>
+        <a href=>create</a>
+        <a href=>read</a>
+        <a href=>update</a>
+
+        
+    </nav>
+</header>
+
+<div style="padding:20px;">
